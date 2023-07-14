dc306c8d940f72f6dc679bf5365602d45b17fa68
sabahrahal <sabahrahal1998@gmail.com>
2023-07-14 19:51:27 +0200

    key

--- app/components/SingleIcons.jsx
+++ app/components/SingleIcons.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export const SingleIcons = ({title, description, image, alt, linkRef}) => {
   return (
-    <div className="card md:w-96 bg-base-100 shadow-xl mx-auto">
+    <div key={title} className="card md:w-96 bg-base-100 shadow-xl mx-auto">
     <figure>
         <Image className="aspect-[16/9] object-cover object-center w-full" src={ image } width={300} height={300} alt={ alt }/>
     </figure>
